e89f8169332ae17e81b50f05b22574075b72835d
kaitlynnchan <kaitlynnchan@outlook.com>
2020-06-13 15:51:52 -0700

    Implemented main menu.
    Work on UI of buttons.

--- app/src/main/java/cmpt276/assign3/assign3game/MainActivity.java
+++ app/src/main/java/cmpt276/assign3/assign3game/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +20,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        playWelcomeScreen();
+        setupButtons();
+        setupMainBackground();
+    }
+
+    private void playWelcomeScreen() {
+        // Implement welcome screen
+    }
+
+    private void setupButtons() {
+        Button btnPlay = findViewById(R.id.buttonPlay);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Setup game activity
+            }
+        });
+
+        Button btnOptions = findViewById(R.id.buttonOptions);
+        btnOptions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Setup options activity
+            }
+        });
+
+        Button btnHelp = findViewById(R.id.buttonHelp);
+        btnHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Setup to help activity
+            }
+        });
+    }
+
+    private void setupMainBackground() {
+        // Implement background based on theme
     }
 }
