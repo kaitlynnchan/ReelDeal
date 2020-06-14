51455e278f1e89c69efec21e54b07e1937e591f1
kaitlynnchan <kaitlynnchan@outlook.com>
2020-06-13 19:30:54 -0700

    Refactored.
    Removed unused imports, added new comment for activity, and
    refactored variables.

--- app/src/main/java/cmpt276/assign3/assign3game/MainActivity.java
+++ app/src/main/java/cmpt276/assign3/assign3game/MainActivity.java
@@ -1,25 +1,18 @@
 package cmpt276.assign3.assign3game;
 
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.shapes.RectShape;
-import android.graphics.drawable.shapes.Shape;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 
 /**
  * Main menu
+ * Displays play, options, and help buttons to navigate screens
  */
 public class MainActivity extends AppCompatActivity {
+    private int buttonBorderID = R.drawable.button_border;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,12 +24,6 @@ public class MainActivity extends AppCompatActivity {
         setupMainBackground();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-    }
-
     private void playWelcomeScreen() {
         // Implement welcome screen
     }
@@ -46,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Setup game activity
-                btnPlay.setBackground(MainActivity.this.getResources().getDrawable(R.drawable.button_border));
+                // Setup game screen
+                btnPlay.setBackground(MainActivity.this.getResources().getDrawable(buttonBorderID));
             }
         });
 
@@ -56,8 +43,8 @@ public class MainActivity extends AppCompatActivity {
         btnOptions.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Setup options activity
-                btnOptions.setBackground(MainActivity.this.getResources().getDrawable(R.drawable.button_border));
+                // Setup options screen
+                btnOptions.setBackground(MainActivity.this.getResources().getDrawable(buttonBorderID));
             }
         });
 
@@ -65,8 +52,8 @@ public class MainActivity extends AppCompatActivity {
         btnHelp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Setup to help activity
-                btnHelp.setBackground(MainActivity.this.getResources().getDrawable(R.drawable.button_border));
+                // Setup to help screen
+                btnHelp.setBackground(MainActivity.this.getResources().getDrawable(buttonBorderID));
             }
         });
     }
