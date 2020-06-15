abc8374a29226b20568298a08ae0f364fe8d1de0
kaitlynnchan <kaitlynnchan@outlook.com>
2020-06-15 13:29:30 -0700

    Added fishing animation.

--- app/src/main/java/cmpt276/assign3/assign3game/WelcomeScreen.java
+++ app/src/main/java/cmpt276/assign3/assign3game/WelcomeScreen.java
@@ -2,6 +2,7 @@ package cmpt276.assign3.assign3game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -10,9 +11,9 @@ import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class WelcomeScreen extends AppCompatActivity {
-    private Animation rotate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,21 +36,34 @@ public class WelcomeScreen extends AppCompatActivity {
 //        rotate = AnimationUtils.loadAnimation(this, R.anim.anim_rotate);
 
         ImageView imgFishingPole = findViewById(R.id.imageFishingPole);
-        rotate = new RotateAnimation(0f, 30f, 50, 50);
+        Animation rotate = new RotateAnimation(0f, 30f, 50, 50);
         rotate.setDuration(2500);
         rotate.setRepeatCount(1);
         rotate.setRepeatMode(Animation.REVERSE);
 //        rotate.setFillAfter(true);
         imgFishingPole.setAnimation(rotate);
-//        imgFishingPole.setVisibility(View.INVISIBLE);
+        imgFishingPole.setVisibility(View.INVISIBLE);
 
-        if(imgFishingPole.getVisibility() == View.INVISIBLE){
-            ImageView imgFishingRod = findViewById(R.id.imageFishingRod);
-            ImageView imgFishingWire = findViewById(R.id.imageFishingWire);
-            imgFishingRod.setVisibility(View.VISIBLE);
-            imgFishingWire.setVisibility(View.VISIBLE);
+        TextView gameTitle = findViewById(R.id.textViewGameTitle);
+        ObjectAnimator moveY = ObjectAnimator.ofFloat(gameTitle, "translationY", -250);
+        moveY.setStartDelay(3000);
+        moveY.setDuration(2000);
+        moveY.start();
 
-        }
+        ObjectAnimator moveX = ObjectAnimator.ofFloat(gameTitle, "translationX", 350);
+        moveX.setStartDelay(3000);
+        moveX.setDuration(2000);
+        moveX.start();
+
+
+
+//        if(imgFishingPole.getVisibility() == View.INVISIBLE){
+//            ImageView imgFishingRod = findViewById(R.id.imageFishingRod);
+//            ImageView imgFishingWire = findViewById(R.id.imageFishingWire);
+//            imgFishingRod.setVisibility(View.VISIBLE);
+//            imgFishingWire.setVisibility(View.VISIBLE);
+//
+//        }
     }
 
     private void setupSkipButton() {
