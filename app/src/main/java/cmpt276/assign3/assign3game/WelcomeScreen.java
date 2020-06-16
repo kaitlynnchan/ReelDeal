ab7d912e5b10645ed5ae05cf3141382b0c5c8722
kaitlynnchan <kaitlynnchan@outlook.com>
2020-06-15 23:22:43 -0700

    Created timer to keep track of time passed.

--- app/src/main/java/cmpt276/assign3/assign3game/WelcomeScreen.java
+++ app/src/main/java/cmpt276/assign3/assign3game/WelcomeScreen.java
@@ -9,8 +9,6 @@ import android.os.Handler;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.RotateAnimation;
-import android.view.animation.ScaleAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -20,6 +18,8 @@ import java.util.TimerTask;
 
 public class WelcomeScreen extends AppCompatActivity {
 
+    private int timer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,71 +27,67 @@ public class WelcomeScreen extends AppCompatActivity {
 
         moveFishingRod();
         setupSkipButton();
-        startTimer();
 
         new Handler().postDelayed(new Runnable() {
             @Override public void run() {
                 Intent i = MainActivity.makeLaunchIntent(WelcomeScreen.this); startActivity(i);
                 finish();
             }
-        }, 10000);
+        }, timer*2);
     }
-
-    private void startTimer() {
-        new Timer().schedule(new TimerTask() {
-            @Override
-            public void run() {
-                View overlay = findViewById(R.id.viewOverlay);
-                overlay.setAlpha((float)0.5);
-
-                ImageView imgFishingPole = findViewById(R.id.imageFishingPole);
-                imgFishingPole.setAlpha(0);
-
-                TextView gameTitle = findViewById(R.id.textViewGameTitle);
-                Animation scale = AnimationUtils.loadAnimation(WelcomeScreen.this, R.anim.anim_zoom);
-                scale.setFillAfter(true);
-                gameTitle.startAnimation(scale);
-
-            }
-        }, 5500);
-    }
-
 
     private void moveFishingRod() {
         ImageView imgFishingPole = findViewById(R.id.imageFishingPole);
-        Animation rotate = new RotateAnimation(0f, 30f, 50, 50);
-        rotate.setDuration(2500);
-        rotate.setRepeatCount(1);
-        rotate.setRepeatMode(Animation.REVERSE);
-        imgFishingPole.setAnimation(rotate);
+        Animation rotate = AnimationUtils.loadAnimation(WelcomeScreen.this, R.anim.anim_rotate);
+        imgFishingPole.startAnimation(rotate);
 
+        timer += 3000;
         TextView gameTitle = findViewById(R.id.textViewGameTitle);
         ObjectAnimator moveY = ObjectAnimator.ofFloat(gameTitle, "translationY", -250);
-        moveY.setStartDelay(3000);
+        moveY.setStartDelay(timer);
         moveY.setDuration(2000);
         moveY.start();
 
         ObjectAnimator moveX = ObjectAnimator.ofFloat(gameTitle, "translationX", 350);
-        moveX.setStartDelay(3000);
+        moveX.setStartDelay(timer);
         moveX.setDuration(2000);
         moveX.start();
 
-        // Move
+        timer += 2000 + 500;
+        moveAfterFishing();
+    }
+
+    private void moveAfterFishing() {
+        final TextView gameTitle = findViewById(R.id.textViewGameTitle);
         ObjectAnimator moveY2 = ObjectAnimator.ofFloat(gameTitle, "translationY", -350);
-        moveY2.setStartDelay(5500);
+        moveY2.setStartDelay(timer);
         moveY2.setDuration(1000);
         moveY2.start();
 
         ObjectAnimator moveX2 = ObjectAnimator.ofFloat(gameTitle, "translationX", 225);
-        moveX2.setStartDelay(5500);
+        moveX2.setStartDelay(timer);
         moveX2.setDuration(1000);
         moveX2.start();
 
         TextView authors = findViewById(R.id.textViewAuthors);
         ObjectAnimator moveAuthorX = ObjectAnimator.ofFloat(authors, "translationX", 1100);
-        moveAuthorX.setStartDelay(5525);
+        moveAuthorX.setStartDelay(timer);
         moveAuthorX.setDuration(1000);
         moveAuthorX.start();
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                View overlay = findViewById(R.id.viewOverlay);
+                overlay.setAlpha((float) 0.5);
+
+                Animation scale = AnimationUtils.loadAnimation(WelcomeScreen.this, R.anim.anim_zoom);
+                scale.setFillAfter(true);
+                gameTitle.startAnimation(scale);
+
+                timer += 1000 + 4000;
+            }
+        }, timer);
     }
 
     private void setupSkipButton() {
